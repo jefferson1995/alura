3a5c19e33971caf0f7600e32fac2984ee69832f6
Jefferson Barbosa <jefferson.bds@hotmail.com>
2024-10-10 08:41:08 -0300

    Alura - Curso: threads com spring - 'Enviando e-mail com concorrência'

--- Aula-Threads/Threads-com-spring/adopet-store-main/adopet-store/src/main/java/br/com/alura/adopetstore/Anotacoes.java
+++ Aula-Threads/Threads-com-spring/adopet-store-main/adopet-store/src/main/java/br/com/alura/adopetstore/Anotacoes.java
@@ -31,6 +31,33 @@ public class Anotacoes {
 //    Lembre-se que o lock otimista é apenas um tipo de lock, e existem outros tipos com diferentes permissões.
 
 
+    //Enviando e-mail com concorrência
+
+    //AULA1
+
+//    como melhorar a experiência do usuário ao criar um pedido em nossa aplicação. O problema era que o envio de e-mail após a criação do pedido demorava 3 segundos, o que deixava a pessoa usuária esperando por muito tempo.
+//    Para resolver isso, aprendemos a utilizar o Asynchronism (Assincronismo) no Spring, que permite criar Threads separadas para executar tarefas independentes.
+//    Ativamos o Asynchronism na nossa classe principal AdopetStoreApplication usando a anotação @EnableAsync. Em seguida, usamos a anotação @Async no método enviar() da classe EmailPedidoRealizado para criar uma nova Thread para o envio de e-mail.
+//    Com essa mudança, o tempo de resposta para a criação do pedido caiu de 3 segundos para 32 milissegundos, tornando a experiência do usuário muito mais rápida e agradável.
+
+
+    //AULA 2
+
+//    aprendemos como o @Async do Spring Boot cria novas threads para executar tarefas de forma assíncrona.
+//    Utilizando o System.out.println para imprimir o nome da thread atual, pudemos observar que o @Async realmente cria uma nova thread para enviar o email, enquanto o controller continua a executar na thread principal.
+//    Também vimos que o Spring Boot, por padrão, utiliza uma arquitetura bloqueante, onde cada requisição é atendida por uma nova thread. Isso pode levar a lentidão se muitas requisições forem feitas ao mesmo tempo.
+//    O @Async nos permite criar uma arquitetura não bloqueante, onde tarefas independentes podem ser executadas em paralelo, liberando a thread principal para atender outras requisições. Isso aumenta a performance da aplicação e proporciona uma experiência mais rápida para os usuários.
+//    Na próxima aula, veremos como configurar a criação de threads utilizando o @Async.
+
+
+    //AULA 3
+
+//    como configurar as threads criadas pelo Spring para enviar e-mails de forma mais eficiente.
+//    Vimos que o Spring cria threads automaticamente com configurações padrão, mas podemos personalizar essas configurações para otimizar o desempenho da nossa aplicação.
+//    Para isso, criamos um pacote e uma classe de configuração chamada AsyncConfiguration, onde definimos um executor usando a classe ThreadPoolTaskExecutor.
+//    O executor é como um "pool" de threads que gerencia a criação e execução das threads de forma eficiente. Podemos configurar o número mínimo e máximo de threads a serem criadas, a capacidade da fila de espera e o prefixo do nome das threads.
+//    Para utilizar esse executor configurado, adicionamos o parâmetro asyncExecutor à anotação @Async na classe que envia os e-mails.
+//    Com essa configuração, podemos controlar o número de threads que são criadas para enviar e-mails, garantindo que a aplicação não seja sobrecarregada e que os e-mails sejam enviados de forma rápida e eficiente.
 
 
 }
