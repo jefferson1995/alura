6279004baebcc7df9b2a2f8abb67a8a148a4c9bb
Jefferson Barbosa <jefferson.bds@hotmail.com>
2024-10-29 08:55:24 -0300

    Alura - Curso: Java: trabalhando com listas e coleções de dados - aulas: 'Coleção de filmes'

---
+++ curso_list_set_maps/Java - trabalhando com listas e coleções de dados/2914-java-screenmatch-listas-colecoes-main/src/br/com/alura/screenmatch/Anotacoes.java
@@ -0,0 +1,22 @@
+package br.com.alura.screenmatch;
+
+public class Anotacoes {
+
+    //Coleção de Filmes
+
+    //AULA 1
+
+//    Nessa aula, aprendemos a trabalhar com listas de objetos no Java, usando a classe ArrayList. Vimos como criar uma lista de filmes, adicionar filmes à lista e acessar os filmes por índice. Também aprendemos sobre o método size() para saber o tamanho da lista e o método get() para acessar um filme específico.
+//    A aula também nos apresentou a importância da Orientação a Objetos no Java, mostrando como a classe ArrayList utiliza conceitos como herança, interfaces e encapsulamento.
+
+    //AULA 2
+
+//    Nessa aula, aprendemos sobre a importância da documentação do Java, o Javadoc, e como ele pode nos ajudar a entender melhor o funcionamento das classes e métodos.
+//    Vimos que a classe Object é a "mãe" de todas as classes em Java, e que todas as classes herdam o método toString(), que retorna uma representação textual do objeto.
+//    Aprendemos também como sobrescrever o método toString() para que ele retorne uma representação personalizada do objeto, e como isso impacta o comportamento de outras classes, como o ArrayList.
+
+
+
+
+
+}
